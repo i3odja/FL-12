db411aad047ffa3ca93e4b89bd19844a7dd04eaa
Bohdan Volykh <wbodjaw@gmail.com>
2019-12-22 10:15:30 +0200

    Update task1.js

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -11,10 +11,8 @@ if (isNaN(a) || isNaN(b) || isNaN(c) || a === 0) {
 } else {
     discriminant = b * b - FOUR_DISCRIMINANT * a * c;
     if (discriminant > 0) {
-        x1 = (-b + Math.sqrt(discriminant)) / (TWO_X_X1_X2 * a);
-        x2 = (-b - Math.sqrt(discriminant)) / (TWO_X_X1_X2 * a);
-        x1 = parseInt(x1, 10);
-        x2 = parseInt(x2, 10);
+        x1 = parseInt((-b + Math.sqrt(discriminant)) / (TWO_X_X1_X2 * a), 10);
+        x2 = parseInt((-b - Math.sqrt(discriminant)) / (TWO_X_X1_X2 * a), 10);
         console.log('x1 = %d and x2 = %d', x1, x2);
     } else if (discriminant === 0) {
         x = -b / (TWO_X_X1_X2 * a);
