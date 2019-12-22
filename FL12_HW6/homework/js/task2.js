bbf2f4dfdb98d350646286a689560bdf87106823
Bohdan Volykh <wbodjaw@gmail.com>
2019-12-22 10:25:52 +0200

    Update task2.js

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -4,7 +4,7 @@ let c = parseInt(prompt('Enter trigle side#3 length:', ''), 10);
 if (isNaN(a) || isNaN(b) || isNaN(c)) {
     alert('input values should be ONLY numbers');
 } else {
-    if (a === 0 || b === 0 || c === 0) {
+    if (a <= 0 || b <= 0 || c <= 0) {
         alert('A triangle must have 3 sides with a positive definite length');
     } else {
         if (a + b > c && a + c > b && b + c > a) {
